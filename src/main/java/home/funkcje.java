b143926a8196bc1090106482ed00da53b374ec22
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2024-04-16 11:28:20 +0200

    new file funkcje

---
+++ src/main/java/home/funkcje.java
@@ -0,0 +1,11 @@
+package home;
+
+public interface funkcje {
+
+    public  double W(int varible);
+    public  double F(int varible);
+    public  int D(int varible);
+    public  double H(int varible);
+    public  double G(int varible);
+    public  double A(int varible);
+}
