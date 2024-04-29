ba068e834de01638939e38422964986f9be6acbf
Zbigniew Gołębiewski <zbigolo@gazeta.pl>
2024-04-29 12:29:54 +0200

    new file

---
+++ src/test/java/school/drukarkaTest.java
@@ -0,0 +1,20 @@
+package school;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class drukarkaTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void w() {
+    }
+
+    @Test
+    void drukuj() {
+    }
+}
